264873b39edf8c6aede063960670fafcc168b1ab
Pavel Nikolaev <pavel.nikolaev@tele2.ru>
2023-08-27 05:37:28 +0300

    Test push

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 console.log('started')
 //
 const test = () => {
-    console.log(10000)
-    setTimeout(() => {
-        test()
-    }, 10000)
+    console.log('ok')
+    // setTimeout(() => {
+    //     test()
+    // }, 10000)
 }
 
 test()
